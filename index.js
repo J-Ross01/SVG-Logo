c9d577daafc95ce06c60d44a7d5bf88ae37309d3
Jarrett Delaney <jarrettdelaney1@gmail.com>
2023-12-13 21:36:50 -0800

    Added the function that generates the SVG string along with notes on code logic.

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ async function importModules() {
 }
 // Asynchronously prompts the user for input using Inquirer.
 async function promptUser() {
+     // Waiting for user to answer the series of quetions. 
     const answers = await inquirer.prompt([
         {
             type: 'input',
             name: 'text',
             message: 'Enter three characters for logo text:',
+            //Ensures that the input is 3 characters or less.
             validate: input => input.length <= 3
         },
         {
@@ -24,6 +26,7 @@ async function promptUser() {
             message: 'Enter a color for the text (keyword/hex):',
         },
         {
+             // Dropdown list to select the shape.
             type: 'list',
             name: 'shape',
             message: 'What shape would you like?:',
@@ -35,6 +38,37 @@ async function promptUser() {
             message: 'Enter a shape color (keyword/hex):',
         }
     ]);
-
+    // Returns the user's answers to be used elsewhere in the script.
     return answers;
 }
+
+// This will generate an SVG string based in the user's input.
+function generateSVG({ text, textColor, shape, shapeColor }) {
+    let shapeObj;
+     // A switch statement to determine which shape object to create and with its sepcified color.
+    switch (shape) {
+        case 'Circle':
+            shapeObj = new Circle(shapeColor);
+            break;
+        case 'Triangle':
+            shapeObj = new Triangle(shapeColor);
+            break;
+        case 'Square':
+            shapeObj = new Square(shapeColor);
+            break;
+        default:
+            throw new Error('Unknown shape type');
+    }
+    // Creates an SVG content string using template literals.
+    // It inputs the shape and text based on the users input.
+    const svgContent = `
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            ${shapeObj.render()}
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}">${text}</text>
+        </svg>
+    `;
+    // Returns the SVG content.
+    return svgContent;
+}
+// Imports the shape classes (Triangle, Circle, Square) from the 'shape.js' file. 
+import { Triangle, Circle, Square } from './lib/shape.js';
